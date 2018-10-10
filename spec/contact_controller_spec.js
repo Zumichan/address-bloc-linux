d153f4980a8f8808a70ecf8de8868e9ea3f2d67e
zumichan <azumi.y.callanan@gmail.com>
2018-10-10 10:11:45 -0700

    CP Creating Contacts: Completed

---
+++ spec/contact_controller_spec.js
@@ -0,0 +1,39 @@
+const ContactController = require("../controllers/ContactController");
+const sequelize = require("../db/models/index").sequelize;
+
+describe("ContactController",()=>{
+
+  //We need new ContactController instance for every test
+  beforeEach((done) => {
+     this.book = new ContactController();
+
+     //Before each test, we need to clear the test entries created
+     sequelize.sync({force: true}).then((res) => {
+       //This is an async operation, we need to call done to indicate that the test is completed
+       done();
+     })
+     .catch((err) => {
+       done();
+     });
+  });
+
+  it("should be defined",()=>{
+    expect(ContactController).toBeDefined();
+  });
+
+  describe("#addContact()", () => {
+
+    it("should add a single contact into the book", (done) => {
+      this.book.addContact("Alice", "001-101-1010")
+      .then((contact) => {
+
+      expect(contact.name).toBe("Alice");
+      expect(contact.phone).toBe("001-101-1010");
+      done();
+      })
+      .catch((err) => {
+        done();
+      });
+    });
+  });
+});
